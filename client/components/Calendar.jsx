d811d674ecf35e5fe470f4a7aad688e523f84065
paul-brabet <pbrabet@gmail.com>
2017-06-15 19:08:06 +1200

    overlap prevention feature works

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -78,23 +78,87 @@ class Calendar extends React.Component {
   render () {
     return (
       <div>
-        <h3 className="callout">
+        <h3 className='callout'>
           Click an event to see more info, or
           drag the mouse over the calendar to select a date/time range.
         </h3>
         <BigCalendar
-          className="calendar"
+          className='calendar'
           selectable
           events={events}
           defaultView='week'
           scrollToTime={new Date(1970, 1, 1, 6)}
           defaultDate={new Date(2015, 3, 12)}
           onSelectEvent={event => alert(event.title)}
-          onSelectSlot={slotInfo => { alert(`selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` + `\nend: ${slotInfo.end.toLocaleString()}`) }}
+          onSelectSlot={slotInfo => {
+            showSlotSelection(slotInfo)
+            const chosenSlots = takeSlotSelection(slotInfo)
+            const takenSlots = mapTakenSlots()
+            // console.log('You chose: ' + chosenSlots)
+            // console.log('Already taken: ' + takenSlots)
+            // console.log(compareSlotSelection(slotInfo))
+            console.log(compareSlotSelection(chosenSlots, takenSlots))
+          }}
         />
       </div>
     )
   }
+}
+
+function showSlotSelection (slotInfo) {
+  alert(`selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` + `\nend: ${slotInfo.end.toLocaleString()}`)
+}
+
+function compareSlotSelection (chosenSlots, takenSlots) {
+  for (let i = 0; i < chosenSlots.length - 1; i++) {
+    for (let j = 0; j < takenSlots.length; j++) {
+      for (let k = 0; k < takenSlots[j].length - 1; k++) {
+        if (chosenSlots[i] === takenSlots[j][k]) {
+          return true
+        }
+      }
+    }
+  }
+}
+
+function startPointEventsForTaken () {
+  startPointsEvents.map(obj => {
+    return obj.start
+  })
+}
+
+function takeSlotSelection (slotInfo) {
+  let slotArr = slotInfo.slots.map(slot => {
+    return moment(slot).format('YYYY-MM-DD hh:mm')
+  })
+  return slotArr
+}
+
+function mapTakenSlots () {
+  let slotArr = events.map(obj => {
+    return intervals(obj.start, obj.end)
+  })
+  return slotArr
+}
+
+function intervals (startTime, endTime) {
+  const start = moment(startTime, 'YYYY-MM-DD hh:mm')
+  const end = moment(endTime, 'YYYY-MM-DD hh:mm')
+
+  // round starting minutes up to nearest 30 (26 --> 30, 32 --> 60)
+  // note that 59 will round up to 60, and moment.js handles that correctly
+  start.minutes(Math.ceil(start.minutes() / 30) * 30)
+
+  const result = []
+
+  const current = moment(start)
+
+  while (current <= end) {
+    result.push(current.format('YYYY-MM-DD HH:mm'))
+    current.add(30, 'minutes')
+  }
+
+  return result
 }
 
 export default Calendar
